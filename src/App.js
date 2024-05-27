e7499e0a2ec60ee8acd590dad4a2d99d31990416
kunal <kuncode2@gmail.com>
2024-05-27 09:44:02 +0530

    Routin part done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
 
 function App() {
   
   return (
     <div>
+      <BrowserRouter>
      <Navbar/>
+     <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory category="men"/>}/>
+      <Route path='/womens' element={<ShopCategory/>}/>
+      <Route path='/kids' element={<ShopCategory/>}/>  
+      <Route path="/product" element={<Product/>}>
+         <Route path=':productId' element={<Product/>}/>  
+      </Route>   
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='login' element={<Cart/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
