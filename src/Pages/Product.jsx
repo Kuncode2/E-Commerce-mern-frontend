e5a7c839b4f5632f3b4a9316f80bda36c1b69e12
kunal <kuncode2@gmail.com>
2024-06-09 19:21:53 +0530

    Product section start, done with paganantion

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,18 +1,20 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
   const { all_product } = useContext(ShopContext); 
   const {productId} = useParams()
   const product = all_product.find((e)=>e.id === Number(productId))// This is done for  while the id is compared to the product then it must be same 
   if (!product) {
-    return <div>Loading...</div>; // or an appropriate loading state
+    return <div>Loading...</div>; 
   }
   return (
     <div> 
       <Breadcrum product={product} />
+      <ProductDisplay product={product}/>
     </div> 
   )
 }
