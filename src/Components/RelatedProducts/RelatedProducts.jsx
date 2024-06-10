153bf4de534795af3dde70c4b54d51cb318d0e5c
kunal <kuncode2@gmail.com>
2024-06-10 16:16:20 +0530

    Product section has done new components start

---
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './RelatedProducts.css'
+import data_product from '../Assets/data'
+const RelatedProducts = () => {
+  return (
+    <div className='relatedproducts'>
+      <h1>Telated Products</h1>
+      <hr />
+      <div className="realtedproducts-item"></div>
+    </div>
+  )
+}
+import './RelatedProducts.css'
+export default RelatedProducts
